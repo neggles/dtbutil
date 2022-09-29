973607b47e10b8496aca663a69cebeae79a8f401
neg2led <4232981+neg2led@users.noreply.github.com>
2022-09-29 18:57:35 +1000

    add trim function

--- src/dtbutil/app.py
+++ src/dtbutil/app.py
@@ -67,11 +67,91 @@ def todts(
             subprocess.check_call(DTC_ARGS + [outfile, path])
         except subprocess.CalledProcessError as e:
             err_console.print(f"Error converting {path} to {outfile}: {e}")
-            carryon = console.input(prompt="Continue? [Y/N] ", emoji=False, default="N")  # type: ignore
+            carryon: str = console.input(prompt="Continue? [Y/N] ", emoji=False)
             if carryon.lower() != "y":
                 raise typer.Abort()
         console.print(" OK", style="bold green")
 
 
-if __name__ == "__main__":
-    app()
+@app.command()
+def trim(
+    infile: List[Path] = typer.Argument(
+        ...,
+        exists=True,
+        file_okay=True,
+        dir_okay=False,
+        readable=True,
+        help="Source device tree blob file(s)",
+    ),
+    backup: bool = typer.Option(
+        False,
+        "--backup",
+        "-b",
+        help="Create a backup of the original file(s) before trimming",
+    ),
+    suffix: str = typer.Option(
+        "bak",
+        "--suffix",
+        "-s",
+        help="Suffix to append to backup files, if --backup is specified",
+    ),
+):
+    for file in infile:
+        if backup:
+            bakfile = Path(f"{file}.{suffix}")
+            if bakfile.exists():
+                err_console.print(
+                    f"Backup file [bold purple]{bakfile}[/] already exists, will not overwrite.",
+                    style="bold red",
+                )
+                continue
+            else:
+                console.print(f"Backing up [bold blue]{file}[/] to [bold purple]{bakfile}[/] ...", end="")
+                try:
+                    bakfile.write_bytes(file.read_bytes())
+                except Exception as e:
+                    err_console.print(f"Error backing up {file} to {bakfile}: {e}")
+                    continue
+                console.print(" OK", style="bold green")
+        else:
+            bakfile = None
+
+        try:
+            console.print(f"Trimming [bold blue]{file}[/] ", end="")
+            with file.open("rb") as f:
+                # check for DTB magic
+                magic = f.read(4)
+                if magic != b"\xd0\x0d\xfe\xed":
+                    err_console.print(
+                        f"File [bold blue]{file}is not a valid device tree blob.", style="bold red"
+                    )
+                    err_console.print(f"Magic number is {magic.hex()}, expected d00dfeed")
+                    continue
+
+                filesize = file.stat().st_size
+                dtbsize = int.from_bytes(f.read(4), "big")
+                console.print(f"to {dtbsize} bytes ...", end="")
+
+                if filesize == dtbsize:
+                    console.print(" no change", style="bold yellow")
+                    if isinstance(backup, Path):
+                        bakfile.unlink()  # type: ignore
+                    continue
+                elif filesize < dtbsize:
+                    err_console.print(" WARNING", style="bold red")
+                    err_console.print(
+                        f"File size ({filesize} bytes) is below {dtbsize} bytes from DTB header, will not trim.",
+                        style="yellow",
+                    )
+                    continue
+                else:
+                    console.print(
+                        f"from [bold purple]{filesize}[/] to [bold green]{dtbsize}[/] bytes ...", end=""
+                    )
+                    f.truncate(dtbsize)
+                    console.print(" OK", style="bold green")
+        except Exception as e:
+            err_console.print(f"Error trimming {file}: {e}")
+            carryon: str = console.input(prompt="Continue? [Y/N] ", emoji=False)
+            if carryon.lower() != "y":
+                raise typer.Abort()
